b3f861be5eb502529120464b782bcad7cda987fc
dexman545 <kev12east@gmail.com>
2020-01-26 15:16:15 -0500

    Update gradle

--- src/main/java/dex/iguanablanket/IguanaBlanketClient.java
+++ src/main/java/dex/iguanablanket/IguanaBlanketClient.java
@@ -6,11 +6,37 @@ import net.fabricmc.fabric.api.event.client.ClientTickCallback;
 import net.fabricmc.fabric.api.event.server.ServerTickCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.LiteralText;
+import spinnery.util.InGameHudScreen;
+import spinnery.widget.*;
 
 public class IguanaBlanketClient implements ClientModInitializer {
 
 	@Override
 	public void onInitializeClient() {
+		InGameHudScreen.addOnInitialize(() -> {
+			// WInterfaceHolder
+			WInterfaceHolder holder = InGameHudScreen.getHolder();
+			// WInterfaceHolder
+
+
+
+			// WInterface
+			WInterface mainInterface = new WInterface(WPosition.of(WType.FREE, 8, 8, 0));
+
+			holder.add(mainInterface);
+			// WInterface
+
+
+
+			// WStaticText
+			WStaticText staticTextA = new WStaticText(WPosition.of(WType.ANCHORED, 0, 0, 0, mainInterface), mainInterface, new LiteralText("StaticText A"));
+			// WStaticText
+
+
+
+			mainInterface.add(staticTextA);
+		});
 		ClientTickCallback.EVENT.register(t -> {
 
 		});
