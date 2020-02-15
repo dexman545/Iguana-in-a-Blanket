76f8eaa3e58fa4c0b4ecbc4d64dabb9c826b3171
dexman545 <kev12east@gmail.com>
2020-02-14 21:17:40 -0500

    Add flying items damage entities

--- src/main/java/dex/iguanablanket/mixin/ItemEntityMixin.java
+++ src/main/java/dex/iguanablanket/mixin/ItemEntityMixin.java
@@ -1,15 +1,30 @@
 package dex.iguanablanket.mixin;
 
+import com.google.common.collect.ImmutableSet;
 import dex.iguanablanket.config.LuaConfigCompilation;
+import dex.iguanablanket.impl.ItemWeight;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ProjectileUtil;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.predicate.entity.EntityPredicates;
+import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.shape.VoxelShape;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.util.Set;
+import java.util.stream.Stream;
 
 @Mixin(ItemEntity.class)
 public abstract class ItemEntityMixin {
@@ -29,5 +44,30 @@ public abstract class ItemEntityMixin {
 
         ((ItemEntity)(Object)this).setVelocity(vec3d.x * 0.9900000095367432D, yVel, vec3d.z * 0.9900000095367432D);
 
+    }
+
+    private static EntityHitResult rayTraceEntity(Entity entity, float partialTicks) {
+        Vec3d from = entity.getPos();
+        Vec3d look = entity.getRotationVec(partialTicks);
+        Vec3d v = entity.getVelocity();
+        Vec3d to = from.add(v);
+
+        return ProjectileUtil.getEntityCollision(entity.world, entity, from, to, (new Box(from, to)).expand(5), EntityPredicates.VALID_ENTITY);
+    }
+
+    @Inject(at = @At("TAIL"), method = "tick()V")
+    private void modifyTick(CallbackInfo ci) {
+        Vec3d v = ((ItemEntity) (Object) this).getVelocity();
+
+
+        EntityHitResult h = rayTraceEntity(((ItemEntity) (Object) this), 1.0F);
+        if (h != null) {
+            Entity e = h.getEntity();
+            if (e instanceof LivingEntity) {
+                ItemStack s = ((ItemEntity)(Object)this).getStack();
+                double amount = ((ItemWeight) (Object) s).getWeight() / v.length();
+                ((LivingEntity)e).damage(DamageSource.FALLING_BLOCK, (float) amount);
+            }
+        }
     }
 }
