4569858600bfd6cbec5c2b2473ead5fec7253ee8
Deximus-Maximus <kev12east@gmail.com>
2020-10-21 16:46:39 -0400

    Fix hardness calc being recursive

--- src/main/java/dex/iguanablanket/mixin/BlockStateMixin.java
+++ src/main/java/dex/iguanablanket/mixin/BlockStateMixin.java
@@ -6,7 +6,9 @@ import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.BlockView;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -14,10 +16,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(AbstractBlock.AbstractBlockState.class)
 public abstract class BlockStateMixin {
 
+	@Shadow @Final private float hardness;
+
 	@Inject(at=@At("HEAD"), method = "getHardness", cancellable = true)
 	public void getHardness(BlockView world, BlockPos pos, CallbackInfoReturnable<Float> cir) {
 		float factor = LuaConfigCompilation.blockhardness.getOrDefault(Registry.BLOCK.getId(((BlockState) (Object) this).getBlock()).toString(), 1f);
-		cir.setReturnValue(((BlockState)(Object)this).getHardness(world, pos) * factor);
+		cir.setReturnValue(this.hardness * factor);
 	}
 
 }
