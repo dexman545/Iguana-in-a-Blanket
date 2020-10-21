ab42f10da9169e2a4a2fb5dd6fc6201948dadd3b
CoolMineman <62723322+CoolMineman@users.noreply.github.com>
2020-10-20 23:33:12 -0500

    oops

--- remappedSrc/dex/iguanablanket/helpers/MathHelper.java
+++
@@ -1,7 +0,0 @@
-package dex.iguanablanket.helpers;
-
-public abstract class MathHelper {
-    public static double clampValue(double value, double min, double max) {
-        return Math.min(Math.max(value, min), max);
-    }
-}
--- remappedSrc/dex/iguanablanket/impl/BedWakeCheck.java
+++
@@ -1,4 +0,0 @@
-package dex.iguanablanket.impl;
-
-public interface BedWakeCheck {
-}
